e6d6035d6763816466fc80b0a042829b734c0695
Brenda Lima <breholiveira07@gmail.com>
2026-09-13 16:19:04 -0300

    feat: rota GET listar imoveis

--- servidor.py
+++ servidor.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import utils
 
 app = Flask(__name__)
+
+
+@app.route("/imoveis", methods=['GET'])
+def listar_imoveis_rota():
+
+    dados = utils.listar_imoveis()
+    return jsonify(dados), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -15,3 +15,26 @@ config = {
 
 def conectar_banco():
     return mysql.connector.connect(**config)
+
+def listar_imoveis():
+    conn = conectar_banco()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM imoveis")
+    resultados = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    imoveis = []
+    for imovel in resultados:
+        imoveis.append({
+            "id": imovel[0],
+            "logradouro": imovel[1],
+            "tipo_logradouro": imovel[2],
+            "bairro": imovel[3],
+            "cidade": imovel[4],
+            "cep": imovel[5],
+            "tipo": imovel[6],
+            "valor": imovel[7],
+            "data_aquisicao": imovel[8]
+        })
+    return imoveis
