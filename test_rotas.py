1fc756113d034358fc26421b59d3ec115e51bd7e
Brenda Lima <breholiveira07@gmail.com>
2026-09-15 09:08:07 -0300

    feat: teste GET lista imoveis por tipo invalido e reorganização dos testes para agrupar por metodos HTTP

--- test_rotas.py
+++ test_rotas.py
@@ -131,6 +131,80 @@ def test_GET_listar_um_imovel_not_found_404(mock_conectar_banco, client):
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
 
+# Teste buscar imóveis por tipo (casa, apartamento, terreno, etc) com todos os seus atributos;
+@patch("utils.conectar_banco")
+def test_GET_listar_imoveis_por_tipo_200(mock_conectar_banco, client):
+    """GET /imoveis - lista com dados."""
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+    mock_cursor.fetchall.return_value = [
+        (1, "Nicole Common", "Travessa", "Lake Danielle", "Judymouth", "85184", "casa em condominio", 488424, "2017-07-29"),
+    ]
+
+    mock_conectar_banco.return_value = mock_conn
+
+    response = client.get("/imoveis/casa em condominio") # substitui pela rota definida no sql do teste
+
+    assert response.status_code == 200
+    assert response.get_json() == [{
+        "id": 1,
+        "logradouro": "Nicole Common", 
+        "tipo_logradouro": "Travessa", 
+        "bairro": "Lake Danielle", 
+        "cidade": "Judymouth", 
+        "cep": "85184", 
+        "tipo": "casa em condominio", 
+        "valor": 488424.0, 
+        "data_aquisicao": "2017-07-29"
+        },
+    ]
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT * FROM imoveis WHERE tipo = %s",
+        ("casa em condominio",)
+    )
+    mock_cursor.fetchall.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
+
+# teste de listar imóveis com lista vazia
+@patch("utils.conectar_banco")
+def test_GET_listar_imoveis_por_tipo_vazio_200(mock_conectar_banco, client):
+    """GET /imoveis - lista vazia."""
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    mock_conn.cursor.return_value = mock_cursor
+    mock_cursor.fetchall.return_value = []
+
+    mock_conectar_banco.return_value = mock_conn
+
+    response = client.get("/imoveis/casa em condominio")
+
+    assert response.status_code == 200
+    assert response.get_json() == []
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT * FROM imoveis WHERE tipo = %s",
+        ("casa em condominio",)
+    )
+    mock_cursor.fetchall.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
+
+# Teste erro ao listar imoveis por tipo invalido
+@patch("utils.conectar_banco")
+def test_GET_listar_imovel_por_tipo_invalido_400(mock_conectar_banco, client):
+    """GET /imovel/<tipo> - tipo inválido -> 400. Não deve acessar o banco."""
+    response = client.get("/imoveis/xxxx")
+
+    assert response.status_code == 400
+    assert response.get_json() == {"erro": "Tipo inválido"}
+
+    mock_conectar_banco.assert_not_called()
+
+# ========================== TESTES - POST ========================================
 
 # Teste adicionar um novo imóvel;
 @patch("utils.conectar_banco")
@@ -179,6 +253,7 @@ def test_POST_criar_imovel_400(mock_conectar_banco, client):
 
     mock_conectar_banco.assert_not_called()
 
+# ========================== TESTES - UPDATE ========================================
 
 # Teste atualizar um imóvel existente;
 @patch("utils.conectar_banco")
@@ -261,6 +336,7 @@ def test_update_imovel_not_found_404(mock_conectar_banco, client):
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
 
+# ========================== TESTES - DELETE ========================================
 
 @patch("utils.conectar_banco")
 def test_deletar_imovel_200(mock_conectar_banco, client):
@@ -306,67 +382,5 @@ def test_deletar_imoveis_not_found_404(mock_conectar_banco, client):
         (999,),
     )
 
-    mock_cursor.close.assert_called_once()
-    mock_conn.close.assert_called_once()
-
-# Teste buscar imóveis por tipo (casa, apartamento, terreno, etc) com todos os seus atributos;
-@patch("utils.conectar_banco")
-def test_GET_listar_imoveis_por_tipo_200(mock_conectar_banco, client):
-    """GET /imoveis - lista com dados."""
-    mock_conn = MagicMock()
-    mock_cursor = MagicMock()
-    mock_conn.cursor.return_value = mock_cursor
-    mock_cursor.fetchall.return_value = [
-        (1, "Nicole Common", "Travessa", "Lake Danielle", "Judymouth", "85184", "casa em condominio", 488424, "2017-07-29"),
-    ]
-
-    mock_conectar_banco.return_value = mock_conn
-
-    response = client.get("/imoveis/casa em condominio") # substitui pela rota definida no sql do teste
-
-    assert response.status_code == 200
-    assert response.get_json() == [{
-        "id": 1,
-        "logradouro": "Nicole Common", 
-        "tipo_logradouro": "Travessa", 
-        "bairro": "Lake Danielle", 
-        "cidade": "Judymouth", 
-        "cep": "85184", 
-        "tipo": "casa em condominio", 
-        "valor": 488424.0, 
-        "data_aquisicao": "2017-07-29"
-        },
-    ]
-
-    mock_cursor.execute.assert_called_once_with(
-        "SELECT * FROM imoveis WHERE tipo = %s",
-        ("casa em condominio",)
-    )
-    mock_cursor.fetchall.assert_called_once()
-    mock_cursor.close.assert_called_once()
-    mock_conn.close.assert_called_once()
-
-# teste de listar imóveis com lista vazia
-@patch("utils.conectar_banco")
-def test_GET_listar_imoveis_por_tipo_vazio_200(mock_conectar_banco, client):
-    """GET /imoveis - lista vazia."""
-    mock_conn = MagicMock()
-    mock_cursor = MagicMock()
-
-    mock_conn.cursor.return_value = mock_cursor
-    mock_cursor.fetchall.return_value = []
-
-    mock_conectar_banco.return_value = mock_conn
-
-    response = client.get("/imoveis/casa em condominio")
-
-    assert response.status_code == 200
-    assert response.get_json() == []
-
-    mock_cursor.execute.assert_called_once_with(
-        "SELECT * FROM imoveis WHERE tipo = %s",
-        ("casa em condominio",)
-    )
-    mock_cursor.fetchall.assert_called_once()
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
